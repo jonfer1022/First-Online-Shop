3474732ab1268be58c2c8e5dd5970c5beea7ff45
jonfer1022 <75956299+jonfer1022@users.noreply.github.com>
2020-12-31 14:17:21 -0500

    Añadiendo más estilos y caracteristicas al header

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,17 @@
-// import logo from './logo.svg';
-// import './App.css';
 import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from './NavBar';
+import { Header } from './Header';
 import { Layout } from './Layout';
-import './styles/Header.scss'
 
 function App() {
   return (
     <Fragment>
       <header>
-        <NavBar></NavBar>
-        <div class="intro">
-          <div class="overlay">
-            <div class="container">
-              <div class="row">
-                <div class="intro-text">
-                  <h1>Touché</h1>
-                  <p>Restaurant / Coffee / Pub</p>
-                  <a href="#about" class="btn btn-custom btn-lg page-scroll">Discover Story</a> 
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <NavBar/>
+        <Header/>
       </header>
+      <div>hola</div>
       <Layout>
         <BrowserRouter>
           <Switch>
