7f66cf0af4ba03b0655a3f6ca58270a5665e5b57
jonfer1022 <75956299+jonfer1022@users.noreply.github.com>
2021-01-01 17:12:43 -0500

    Imagen nueva en el header y llamado del Header desde un archivo nuevo

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles/Header.scss'
+
+export const Header = (props) => (
+  <div class="intro">
+    <div class="overlay">
+      <div class="container">
+        <div class="row">
+          <div class="intro-text">
+            <h1>Samplhes</h1>
+            <p>Ropa a tu medida</p>
+            <a href="#about" class="btn-custom btn-lg page-scroll">¡Descubre lo último!</a> 
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+)
