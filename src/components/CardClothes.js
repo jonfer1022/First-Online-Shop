3b4a3e5789ce1d9b629f1fe3a2603c9b55b32ce9
jonfer1022 <75956299+jonfer1022@users.noreply.github.com>
2021-01-06 08:52:47 -0500

    archivos nuevos para el funcionamiento del anterior commit

---
+++ src/components/CardClothes.js
@@ -0,0 +1,33 @@
+import React, { Fragment } from 'react';
+import { Card, Image } from 'react-bootstrap';
+import './styles/CardClothes.scss';
+// import RopaImg from '../img/img-portafolio/01-small.jpg';
+
+const CardClothes = (data) =>{
+
+    // console.log(RopaImg);
+
+    // Mapeamos el arreglo data que contiene los productos y crea un Card por cada producto.
+    return (
+        <Fragment>
+            {data.clothes.map((product) => (
+            <Card className="card div-img"
+            key={product.id}   //id de cada Card 
+            border="light">
+                <Card.Header className="textTitle">{product.producto}</Card.Header>
+                <a href={"/producto"} >
+                    <Image src={product.imgRopa} className="img-responsive" rounded/>
+                </a>
+                <div className="div-footer">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.
+                </div>
+                {/* <Card.Footer className="card-info">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed dapibus leonec.
+                </Card.Footer> */}
+            </Card>
+            ))}
+        </Fragment>
+    )
+}
+
+export default CardClothes;
