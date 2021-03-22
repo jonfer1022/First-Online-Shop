bc4e26fa1e9c90533a43174a4fe9d21a64eed7dd
jonfer1022 <75956299+jonfer1022@users.noreply.github.com>
2021-03-21 22:10:09 -0500

    mas modificaciones v2

---
+++ src/master_data.js
@@ -0,0 +1,28 @@
+export default {
+  sides:{
+    home: "Home",
+    products: "Products"
+  },
+
+  clothes_gender:{
+    female: "female",
+    male: "male"
+  },
+
+  clothes_categories:{
+    jackets: 1,
+    sweater: 2,
+    shirts: 3,
+    dresses: 4,
+    shoes: 5
+  },
+
+  clothes_sizes:{
+    all: 0,
+    x_small: 1,
+    small: 2,
+    middle: 3,
+    large: 4,
+    x_large: 5
+  }
+}
